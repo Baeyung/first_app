b9f131e6cb681da1109e4ce1b2af65640f08c435
Baeyung <ahmadsheikh617@gmail.com>
2021-09-03 04:58:52 +0500

    completed part 1 of flutter code lab

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordpair = WordPair.random();
     return MaterialApp(
       title: "Welcome to Flutter",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("MyApp"),
-        ),
-        body: Center(
-          child: Text(wordpair.asPascalCase),
-        ),
-      ),
+      home: RandomWords(),
     );
   }
 }
